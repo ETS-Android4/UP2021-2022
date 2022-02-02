3f27ec4cd613e1be7a723f4fd6bdea8b1b9730a7
c23sd <c23sd@dalton.org>
2022-02-02 15:41:25 -0500

    test servo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankTeleOp.java
@@ -15,8 +15,8 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
         private MecanumRobot rb = new MecanumRobot();
 
-        double liftmotorStartingPosition;
-        double servoBoxStartingPosition = 0;
+        double liftmotorStartingPosition = rb.liftmotor.getCurrentPosition();
+        double servoBoxStartingPosition = rb.boxServo.getPosition();
 
         double frontLeftPower;
         double backLeftPower;
@@ -51,6 +51,8 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
             liftEncoder();
 
+            servoBox();
+
             telemetry.update();
         }
 
@@ -71,7 +73,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
         private void liftEncoder() {
             double x = rb.liftmotor.getCurrentPosition();
-            if(gamepad1.dpad_right && x < liftmotorStartingPosition + 1000){
+            if(gamepad1.dpad_right && x < liftmotorStartingPosition + 400){
                 rb.liftmotor.setPower(1);
             }
             if(gamepad1.dpad_left && x > liftmotorStartingPosition){
@@ -80,6 +82,17 @@ import com.qualcomm.robotcore.util.ElapsedTime;
             else {
                 rb.liftmotor.setPower(0);
             }
+        }
+
+        private void servoBox() {
+            if(gamepad2.dpad_down) {
+                rb.boxServo.setPosition(servoBoxStartingPosition - 0.6);
+
+            }
+            if(gamepad2.dpad_right) {
+                rb.boxServo.setPosition(servoBoxStartingPosition - 0.25);
+            }
+
         }
 
 
@@ -129,38 +142,6 @@ import com.qualcomm.robotcore.util.ElapsedTime;
             rb.blMotor.setPower(backLeftPower);
             rb.frMotor.setPower(frontRightPower);
             rb.brMotor.setPower(backRightPower);
-
-
-//        double r = Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y);
-//        double robotAngle = Math.atan2(gamepad1.left_stick_y, gamepad1.left_stick_x) - Math.PI / 4;
-//        double rightX = gamepad1.right_stick_x;
-//        final double v1 = r * Math.cos(robotAngle) + rightX;
-//        final double v2 = r * Math.sin(robotAngle) - rightX;
-//        final double v3 = r * Math.sin(robotAngle) + rightX;
-//        final double v4 = r * Math.cos(robotAngle) - rightX;
-//
-//        rb.flMotor.setPower(v1);
-//        rb.blMotor.setPower(v2);
-//        rb.frMotor.setPower(v3);
-//        rb.brMotor.setPower(v4);
-
-
-//        float leftY = -gamepad1.left_stick_y;
-//        float leftX = gamepad1.left_stick_x;
-//        float rightX = gamepad1.right_stick_x;
-//
-//        double pow;
-//        if (gamepad1.right_trigger >= TRIGGER_THRESHOLD) {
-//            pow = DRIVE_POWER_SLOW;
-//        } else {
-//            pow = DRIVE_POWER;
-//        }
-//
-//        if (leftX * leftX + leftY * leftY >= DRIVE_STICK_THRESHOLD_SQUARED || Math.abs(rightX) >= DRIVE_STICK_THRESHOLD) {
-//            rb.drive(leftX, leftY, rightX, pow);
-//        } else {
-//            rb.driveStop();
-//        }
         }
     }
 
