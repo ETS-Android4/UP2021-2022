361f61a34c2686ce38d2512e3c17cf4e5c099eda
c23sd <c23sd@dalton.org>
2022-02-02 17:53:00 -0500

    test servo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankTeleOp.java
@@ -53,7 +53,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
             intake();
 
-            liftEncoder();
+//            liftEncoder();
 
             servoBox();
 
@@ -75,25 +75,28 @@ import com.qualcomm.robotcore.util.ElapsedTime;
             }
         }
 
-        private void liftEncoder() {
-            double x = rb.liftmotor.getCurrentPosition();
-            if(gamepad1.dpad_right && x < liftmotorStartingPosition + 400){
-                rb.liftmotor.setPower(1);
-            }
-            if(gamepad1.dpad_left && x > liftmotorStartingPosition){
-                rb.liftmotor.setPower(-1);
-            }
-            else {
-                rb.liftmotor.setPower(0);
-            }
-        }
+//        private void liftEncoder() {
+//            double x = rb.liftmotor.getCurrentPosition();
+//            if(gamepad1.dpad_right && x < liftmotorStartingPosition + 400){
+//                rb.liftmotor.setPower(1);
+//            }
+//            if(gamepad1.dpad_left && x > liftmotorStartingPosition){
+//                rb.liftmotor.setPower(-1);
+//            }
+//            else {
+//                rb.liftmotor.setPower(0);
+//            }
+//        }
 
         private void servoBox() {
             if(gamepad2.dpad_down) {
                 rb.boxServo.setPosition(servoBoxStartingPosition - 0.6);
             }
-            if(gamepad2.dpad_right) {
+            else if(gamepad2.dpad_right) {
                 rb.boxServo.setPosition(servoBoxStartingPosition - 0.25);
+            }
+            else {
+                rb.boxServo.setPosition(servoBoxStartingPosition);
             }
 
         }
@@ -122,16 +125,16 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 
             if(Math.abs(y) > Math.abs(rx)){
-                frontLeftPower = rx;
-                backLeftPower = rx;
-                frontRightPower = -rx;
-                backRightPower = -rx;
-            }
-            else if(Math.abs(y) < Math.abs(rx)){
                 frontLeftPower = y;
                 backLeftPower = y;
                 frontRightPower = y;
                 backRightPower = y;
+            }
+            else if(Math.abs(y) < Math.abs(rx)){
+                frontLeftPower = rx;
+                backLeftPower = rx;
+                frontRightPower = -rx;
+                backRightPower = -rx;
             }
 
             else {
